9f2847c90e64dcb5a2c441842393c47d17c76fc1
Jaime Rump <jaime.rump@trengine.com>
2014-04-24 13:23:00 -0400

    Implement javascript-game communication

--- Web/gamesite/urls.py
+++ Web/gamesite/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
      url(r'^scoreboard/$', 'scoreboard.views.scoreboard', name='scoreboard'),
      url(r'^registration/$', 'scoreboard.views.registration', name='registration'),
      url(r'^register/$', 'scoreboard.views.register', name='register'),
+	 url(r'^record_kill/$', 'scoreboard.views.record_kill', name='record_kill'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- Web/scoreboard/views.py
+++ Web/scoreboard/views.py
@@ -7,7 +7,7 @@ import random
 
 def index(request):
     # Displays the login form
-    player_id = request.session['player_id']
+    player_id = request.session['player_id'] if "player_id" in request.session else None
     if player_id:
 	return HttpResponseRedirect('/scoreboard/')
     return render(request, 'scoreboard/index.html')
@@ -31,17 +31,23 @@ def logout(request):
 
 def scoreboard(request):
     # Displays the scoreboard page to logged in users
-    player_id = request.session['player_id']
+    player_id = request.session['player_id'] if "player_id" in request.session else None
     if not player_id:
-	return HttpResponseRedirect('/')
+		return HttpResponseRedirect('/')
     current_player = Player.objects.get(player_id=player_id)
+	
+	# Get game data
+	#game_id = request.GET['game_id'] if "game_id" in request.GET else 1
+	#game = Game.objects.get(game_id=game_id)
+	#game_records = GameEvents.objects.get(game=game)
+	
     context = {'player':current_player}
     return render(request, 'scoreboard/scoreboard.html', context)
 
 def registration(request):
     # Displays the registration form
-    player_id = request.session['player_id']
-    if player_id:
+    player_id = request.session['player_id'] if "player_id" in request.session else None
+		if player_id:
 	HttpResponseRedirect('/scoreboard/')
     return render(request, 'scoreboard/registration.html')
 
@@ -52,7 +58,7 @@ def register(request):
     confirm_password = request.POST['confirm_password']
 
     if not( confirm_password == password ):
-	return render(request, 'scoreboard/registration.html', {'error_message':"Password confirmation doesn't match."})
+		return render(request, 'scoreboard/registration.html', {'error_message':"Password confirmation doesn't match."})
 
     salt = random.randint(0, 1000000)
     hash = Player.hash_password(password, salt)
@@ -64,3 +70,27 @@ def register(request):
     request.session['player_id'] = fetched_player.player_id
 
     return HttpResponseRedirect('/scoreboard/')
+	
+def record_kill(request):
+	# Records a kill in the database
+	victim_id = request.GET['victim_id']
+	killer_id = request.GET['killer_id']
+	game_id = request.GET['game_id']
+	
+	victim = Player.objects.get(player_id=victim_id)
+	killer = Player.objects.get(player_id=killer_id)
+	game = Game.objects.get(game_id=game_id)
+	
+	# Check if the killer already has a record for this game
+	kill_record = GameEvent.objects.get(game=game, player=killer)
+	if( kill_record == None ):
+		kill_record = GameEvent.(game=game, player=killer, kills=0, deaths=0)
+	kill_record.kills += 1
+	kill_record.save
+	
+	# Check if the victim already has a record for this game
+	death_record = GameEvent.objects.get(game=game, player=death)
+	if( death_record == None ):
+		death_record = GameEvent.(game=game, player=victim, kills=0, deaths=0)
+	death_record.deaths += 1
+	death_record.save
